8f26cc77b31a4c99e4336f1b5024960e640a3f25
Theodor Tonum <theodor@tonum.no>
2014-02-04 17:51:05 +0100

    Bump version

--- lib/klarna/checkout/version.rb
+++ lib/klarna/checkout/version.rb
@@ -1,5 +1,5 @@
 module Klarna
   module Checkout
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 end
--- spec/lib/klarna/checkout/version_spec.rb
+++ spec/lib/klarna/checkout/version_spec.rb
@@ -1,5 +1,5 @@
 require 'klarna/checkout/version'
 
 describe Klarna::Checkout::VERSION do
-  it { should eq '0.0.3' }
+  it { should eq '0.0.4' }
 end
