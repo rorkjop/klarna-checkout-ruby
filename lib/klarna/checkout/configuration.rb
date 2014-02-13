882680e4e2dad610973bab2fb7490378e0b15f22
Theodor Tonum <theodor@tonum.no>
2014-02-13 18:52:27 +0100

    Fixes some issues with the configuration module

--- lib/klarna/checkout/configuration.rb
+++ lib/klarna/checkout/configuration.rb
@@ -6,7 +6,7 @@ module Klarna
       end
 
       def shared_secret
-        @@shared_secret
+        @@shared_secret rescue nil
       end
 
       def merchant_id=(merchant_id)
@@ -14,11 +14,16 @@ module Klarna
       end
 
       def merchant_id
-        @@merchant_id
+        @@merchant_id rescue nil
       end
 
       def configure(&blk)
         yield(self)
+      end
+
+      def reset_configuration!
+        @@shared_secret = nil
+        @@merchant_id   = nil
       end
     end
   end
--- spec/lib/klarna/checkout_spec.rb
+++ spec/lib/klarna/checkout_spec.rb
@@ -3,14 +3,37 @@ require 'json'
 
 describe Klarna::Checkout do
   describe ".configure" do
-    context "after I have configured the module" do
-      Klarna::Checkout.configure do |config|
-        config.shared_secret = 'mysecret'
-        config.merchant_id   = '1337'
+    context "without configuration" do
+      describe "accessing the variables shouldn't blow up" do
+        specify do
+          expect { Klarna::Checkout.shared_secret }.to_not raise_error
+        end
+
+        specify do
+          expect { Klarna::Checkout.merchant_id }.to_not raise_error
+        end
       end
     end
 
-    its(:shared_secret) { should eq 'mysecret' }
-    its(:merchant_id)   { should eq '1337' }
+    context "after I have configured the module" do
+      before(:each) do
+        Klarna::Checkout.configure do |config|
+          config.shared_secret = 'mysecret'
+          config.merchant_id   = '1337'
+        end
+      end
+
+      its(:shared_secret) { should eq 'mysecret' }
+      its(:merchant_id)   { should eq '1337' }
+
+      context "after resetting the configuration" do
+        before(:each) do
+          Klarna::Checkout.reset_configuration!
+        end
+
+        its(:shared_secret) { should eq nil }
+        its(:merchant_id)   { should eq nil }        
+      end
+    end
   end
 end
