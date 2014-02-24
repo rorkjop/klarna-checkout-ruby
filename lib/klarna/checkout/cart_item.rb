a0c6be71fc9287a9108b69aed899eca30bf2b399
Theodor Tonum <theodor@tonum.no>
2014-02-24 13:48:08 +0100

    Add validation to mandatory attributes

--- lib/klarna/checkout/cart_item.rb
+++ lib/klarna/checkout/cart_item.rb
@@ -1,9 +1,15 @@
+require 'active_model'
+
 module Klarna
   module Checkout
     class CartItem < Resource
+      include ActiveModel::Validations
+
       attr_accessor :type, :ean, :reference, :name, :uri, :image_uri, :quantity,
                     :unit_price, :total_price_excluding_tax, :total_tax_amount,
                     :total_price_including_tax, :discount_rate, :tax_rate
+
+      validates_presence_of :reference, :name, :quantity, :unit_price, :tax_rate
 
       def as_json
         json_sanitize({
