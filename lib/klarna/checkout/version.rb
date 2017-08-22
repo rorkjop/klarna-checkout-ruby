fc5fa8d39fe5dc5cf565e5c3c8d492a17f767755
Lars O. Overskeid <ovelar@gmail.com>
2017-08-22 15:07:36 +0200

    Fix dependencies and bump to 1.1.5

--- lib/klarna/checkout/version.rb
+++ lib/klarna/checkout/version.rb
@@ -1,5 +1,5 @@
 module Klarna
   module Checkout
-    VERSION = "1.1.4"
+    VERSION = "1.1.5"
   end
 end
--- spec/lib/klarna/checkout/version_spec.rb
+++ spec/lib/klarna/checkout/version_spec.rb
@@ -1,5 +1,5 @@
 require 'klarna/checkout/version'
 
 describe Klarna::Checkout::VERSION do
-  it { should eq '1.1.4' }
+  it { should eq '1.1.5' }
 end
